6438e091c56ce241c7cb93e187384af609e58d6c
Ardyan Surya <lin.ardyan@gmail.com>
2018-01-16 18:07:48 +0700

    Input Name, Show Name

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -5,6 +5,8 @@
  */
 package hellogit;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Student
@@ -15,7 +17,12 @@ public class HelloGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+    Scanner input = new Scanner(System.in);
+    System.out.print("Input your name : ");
+    String name = input.nextLine();
+    System.out.println("Hello " + name);
+    
     }
     
 }
